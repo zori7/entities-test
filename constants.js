4b73da444ee1b9a094a5999fa5900de030b901c5
Zori <zori.yeghikyan@gmail.com>
2023-08-15 21:07:21 +0400

    changes in search results function

--- constants.js
+++ constants.js
@@ -1,12 +1,12 @@
 const channels = [
-    "https://Linkedin.com/",
-    "https://TikTok.com/",
-    "https://Youtube.com/",
-    "https://Instagram.com/",
-    "https://Twitter.com/",
-    "https://Facebook.com/",
-    "https://Reddit.com/",
-    "https://Pinterest.com/"
+    "Linkedin",
+    "TikTok",
+    "Youtube",
+    "Instagram",
+    "Twitter",
+    "Facebook",
+    "Reddit",
+    "Pinterest"
 ]
 
 module.exports = {
--- getSearchResults.js
+++ getSearchResults.js
@@ -6,13 +6,13 @@ require('dotenv').config()
 
 const search = new SerpApi.GoogleSearch(process.env.SERP_API_KEY);
 
-const searchOnGoogleByKeyword = async (keyword) => {
+const googleKeywordSocialResultsCounts = async (keyword) => {
     const keywordCountOnGoogle = {}
 
     const searchPromises = channels.map(async (channel) => {
         const params = {
             engine: "google",
-            q: `site:${channel} intitle:${keyword}`,
+            q: `site:https://${channel}.com intitle:"${keyword}"`,
             google_domain: "google.com",
             gl: "us",
             hl: "en"
@@ -20,7 +20,7 @@ const searchOnGoogleByKeyword = async (keyword) => {
 
         return new Promise((resolve) => {
             search.json(params, (data) => {
-                keywordCountOnGoogle[channel] = data.search_information.total_results;
+                keywordCountOnGoogle[channel.toLowerCase()] = data.search_information.total_results;
                 resolve();
             });
         });
@@ -33,11 +33,11 @@ const searchOnGoogleByKeyword = async (keyword) => {
 
 (async function() {
 
-    const result = await searchOnGoogleByKeyword('impossible')
+    const result = await googleKeywordSocialResultsCounts('impossible')
 
     console.log(result)
 })()
 
 module.exports = {
-    searchOnGoogleByKeyword
+    googleKeywordSocialResultsCounts
 }
